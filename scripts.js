b0e85716a0fdbe2a06157d8ab6eee44ca51bd071
Vendyss-03 <64092106+Vendyss-03@users.noreply.github.com>
2020-04-23 17:56:33 +0200

    Uploaded index, style and scripts

---
+++ scripts.js
@@ -0,0 +1,98 @@
+let name = 'Adam';
+
+console.log(name + "'s apples")
+console.log('Hello world!')
+
+$('h1').text('We are having a 10 min break until 17:05')
+
+//This is a JavaScript comment ctrl+K+C
+
+/*
+    This is a multiline
+    JavaScript comment
+*/
+
+// let age
+// undefined
+// age = 33
+// 33
+// age+5
+// 38
+// let name = Adam
+// VM653:1 Uncaught ReferenceError: Adam is not defined
+//     at <anonymous>:1:12
+// (anonymous) @ VM653:1
+// let name = "Adam"
+// undefined
+// name
+// "Adam"
+// "Adam's apples"
+// "Adam's apples"
+// name = "Peti"
+// "Peti"
+// name
+// "Peti"
+// let name = 'something'
+// undefined
+// age
+// 33
+// true
+// true
+// false
+// false
+// let oldEnoughToDrive = false
+// undefined
+// oldEnoughToDrive = true
+// true
+// age = 12
+// 12
+// name + 'else'
+// "somethingelse"
+// name + 'else' + name + age
+// "somethingelsesomething12"
+// 'adam' - 'apple'
+// NaN
+// true && true
+// true
+// false || true
+// true
+// true && (false || (true && false))
+// false
+// oldEnoughToDrive
+// true
+// !oldEnoughToDrive
+// false
+// let color1 = 'orange'
+// undefined
+// let color2 = 'blue'
+// undefined
+// let colors = ['orange', 'blue', 'green', 'purple']
+// undefined
+// colors
+// (4) ["orange", "blue", "green", "purple"]
+// colors [0]
+// "orange"
+// colors [3]
+// "purple"
+// colors.length
+// 4
+// colors.push('yellow')
+// 5
+// colors
+// (5) ["orange", "blue", "green", "purple", "yellow"]
+// [12, 54, 1212]
+// (3) [12, 54, 1212]
+// [true, false, true]
+// (3) [true, false, true]
+// [colors, colors, colors]
+// (3) [Array(5), Array(5), Array(5)]
+// colors
+// (5) ["orange", "blue", "green", "purple", "yellow"]
+// colors[2] = 'lime'
+// "lime"
+// colors
+// (5) ["orange", "blue", "lime", "purple", "yellow"]
+// [33, 'Adam', true]
+// (3) [33, "Adam", true]
+
+
